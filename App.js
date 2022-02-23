37b9144bf89c0f30eba5d1a6f8e91db70048d318
Sterenn35 <slh35@hotmail.fr>
2022-02-23 17:51:58 +0100

    Initialisation du projet

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+
+      <Text style={styles.instructions} >
+      To share a photo from your phone with a friend, just press the button below!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  }, 
+});
