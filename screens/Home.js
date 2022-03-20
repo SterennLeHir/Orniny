55cb526d37983d615dfd7b6e9c33290842b2e724
VivienViens <vivu.julu@gmail.com>
2022-03-20 12:04:21 +0100

    Ajout des cases Quiz et Sport

--- screens/Home.js
+++ screens/Home.js
@@ -66,11 +66,45 @@ export default function HomeScreen({ navigation }) {
       
       <View style={styles.PartieGauche}>
       <View style={styles.Quiz}>
-      <Text>Quiz</Text>
+
+      <View style={styles.case}>
+
+      <View style={styles.caseTitre}>
+        <Text style={[styles.titre,styles.vert]}>Q</Text>
+        <Text style={[styles.titre,styles.bleu]}>u</Text>
+        <Text style={[styles.titre,styles.rouge]}>i</Text>
+        <Text style={[styles.titre,styles.jaune]}>z</Text>
+        </View>
+
+      <Text style={styles.desc}>Voici le quiz du jour. Répond bien pour gagner des points !</Text>
+      
+      <View style={styles.bouton}>
+        <Text style={styles.textButton}>Jouer</Text>
+      </View>
+      </View>
 
       </View>
+
       <View style={styles.Sport}>
-        <Text>Sport</Text>
+      <View style={styles.case}>
+
+        <View style={styles.caseTitre}>
+        <Text style={[styles.titre,styles.vert]}>L</Text>
+        <Text style={[styles.titre,styles.bleu]}>e </Text>
+        <Text style={[styles.titre,styles.rouge]}>s</Text>
+        <Text style={[styles.titre,styles.jaune]}>p</Text>
+        <Text style={[styles.titre,styles.vert]}>o</Text>
+        <Text style={[styles.titre,styles.bleu]}>r</Text>
+        <Text style={[styles.titre,styles.jaune]}>t</Text>
+        </View>
+
+        <Text style={styles.desc}>Fais du sport pour gagner encore plus de points !</Text>
+
+        <View style={styles.bouton}>
+          <Text style={styles.textButton}>Faire du sport</Text>
+        </View>
+
+        </View>
 
       </View>
       
@@ -79,7 +113,7 @@ export default function HomeScreen({ navigation }) {
       <View style={styles.Milieu} ></View>
 
       
-        <DraxView
+      <DraxView
           style={styles.Droit}
           receivingStyle={styles.receiving}
           renderContent={({ viewState }) => {
@@ -123,27 +157,34 @@ const styles = StyleSheet.create({
   },
   PartieGauche: {
     height: "100%",
-    flexGrow:1,
+    justifyContent: "space-around",
+    width: "33%",
+    alignItems: "center",
   },
   Quiz: {
-    width: "100%",
-    height: "50%",
-    justifyContent: "center",
+    width: "90%",
+    height: "45%",
+    justifyContent: "space-around",
     alignItems: "center",
+    backgroundColor: "rgba(68,73,123,0.8)",
+    borderRadius: 10,
   },
   Sport: {
-    width: "100%",
-    height: "50%",
-    justifyContent: "center",
+    width: "90%",
+    height: "45%",
+    justifyContent: "space-around",
     alignItems: "center",
+    backgroundColor: "rgba(68,73,123,0.8)",
+    borderRadius: 10,
   },
   Milieu: {
     flexGrow:1,
     height: "100%",
+    width: "33%",
   },
   Droit:{
-    flexGrow:1,
     height: "50%",
+    width: "34%",
     alignSelf:"flex-end",
   },
   imageFond: {
@@ -175,6 +216,68 @@ const styles = StyleSheet.create({
     flex:1,
   },
 
+  // View case :
+
+  case:{
+    width: "90%",
+    height: "90%",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderRadius: 10,
+    
+  },
+  caseTitre:{
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    alignContent:"stretch",
+  },
+
+  // Boutons :
+
+  bouton: {
+    width: "50%",
+    height: "20%",
+    borderRadius: 10,
+    borderColor:"rgb(255,251,162)",
+    borderWidth: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  // Texte :
+  titre:{
+    fontFamily:"Pacifico",
+    fontSize: 36,
+  },
+  desc:{
+    fontFamily:"Noto Sans",
+    fontSize: 20,
+    color: "white",
+  },
+  textButton:{
+    fontFamily:"Pacifico",
+
+    fontSize: 18,
+    color:"rgb(255,251,162)",
+  },
+
+  // Couleurs :
+
+  vert:{
+    color:"rgb(87,241,167)",
+  },
+  bleu:{
+    color:"rgb(122,213,252)",
+  },
+  rouge:{
+    color:"rgb(245,123,123)",
+  },
+  jaune:{
+    color:"rgb(255,251,162)",
+  },
+
+  // Styles pour le dragNdrop :
   
   centeredContent: {
     justifyContent: 'center',
@@ -194,10 +297,6 @@ const styles = StyleSheet.create({
   received: {
     marginTop: 10,
     fontSize: 18,
-  },
-  palette: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
   },
   draggableBox: {
     width: 60,
