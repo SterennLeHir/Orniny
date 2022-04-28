4e406d7ef47d5be5238528d614179d8d4efd714c
VivienViens <vivu.julu@gmail.com>
2022-04-28 18:13:36 +0200

    Rectification

--- screens/Home.js
+++ screens/Home.js
@@ -181,9 +181,18 @@ const widthPoids = counterPoids.interpolate({
       // Analyse du repas
       if(Orniny.variete.length > 3) Orniny.ptsPhysique = Orniny.ptsPhysique - 10; // si Orniny a mangé + de 3 choses
       if(Orniny.variete.length < 2) Orniny.ptsPhysique = Orniny.ptsPhysique - 10; // si Orniny a mangé - de 2 choses
-      if(nbEntree > 1) Orniny.ptsPhysique = Orniny.ptsPhysique - 3*nbEntree;
-      if(nbPlat > 1) Orniny.ptsPhysique = Orniny.ptsPhysique - 10*nbPlat;
-      if(nbDessert > 1) Orniny.ptsPhysique = Orniny.ptsPhysique - 7*nbDessert;
+      if(nbEntree > 1) {
+        Orniny.ptsPhysique = Orniny.ptsPhysique - 3*nbEntree;
+        Orniny.ptsMental = Orniny.ptsMental - 10*nbEntree ;
+      }
+      if(nbPlat > 1) {
+        Orniny.ptsPhysique = Orniny.ptsPhysique - 10*nbPlat;
+        Orniny.ptsMental = Orniny.ptsMental - 10*nbPlat ;
+      }
+      if(nbDessert > 1) {
+        Orniny.ptsPhysique = Orniny.ptsPhysique - 7*nbDessert;
+        Orniny.ptsMental = Orniny.ptsMental - 10*nbDessert ;
+      }
       if (nbPlat == 0) Orniny.ptsPhysique = Orniny.ptsPhysique - 20;
       if (Orniny.sasiete > 100) Orniny.ptsPhysique = Orniny.ptsPhysique - 20; // si Orniny a trop mangé
       // rectification des valeurs 
@@ -229,7 +238,7 @@ const widthPoids = counterPoids.interpolate({
           }
         }
       }
-      if (Orniny.ptsPhysique <= 20 ){
+      if (Orniny.ptsPhysique < 20 ){
         if (Orniny.sasiete > 50) Orniny.poids += 2; 
         else {
           Orniny.poids -= 2;
@@ -253,25 +262,7 @@ const widthPoids = counterPoids.interpolate({
     }
     if (Orniny.ptsMental < 60){
       Orniny.bonheur = Orniny.bonheur - (100 - Orniny.ptsMental)*0.01;
-      // Remise à 0 si nécessaire (pour les tests)
-      if (Orniny.sante >= 100) Orniny.sante = 0 ;
-      if (Orniny.bonheur >= 100) Orniny.bonheur = 0 ;
-      if (Orniny.sasiete >= 100) Orniny.sasiete = 0 ;
-      // actualisation des compteurs
-      if (Orniny.ptsPhysique >= 60){
-        Orniny.sante = Orniny.sante + Orniny.ptsPhysique*0.1;
-      }
-      if (Orniny.ptsPhysique < 60){
-        Orniny.sante = Orniny.sante - (100 - Orniny.ptsPhysique)*0.01;
-      }
-      if (Orniny.ptsMental >= 60){
-        Orniny.bonheur = Orniny.bonheur + Orniny.ptsMental*0.1;
-        
-      }
-      if (Orniny.ptsMental < 60){
-        Orniny.bonheur = Orniny.bonheur - (100 - Orniny.ptsMental)*0.1;
-        
-      }
+    }
       if (Orniny.sante < 10) Orniny.bonheur -= 2; // Orniny ne peut être heureux s'il est en mauvaise santé
       if (Orniny.bonheur < 10) Orniny.sante -= 2; // Orniny ne peut pas être en bonne santé s'il n'est pas heureux
       setCompteurPhy(Orniny.sante);
@@ -290,7 +281,7 @@ const widthPoids = counterPoids.interpolate({
       else if (Orniny.poids > 80) { setSpriteOrniny(OrninyIdeal) ; Orniny.image = OrninyIdeal ; }
       else { setSpriteOrniny(OrninyMaigre) ; Orniny.image = OrninyMaigre ; }
     }    
-  }
+  
 
   function repos() {
     dodo ? setDodo(false) : setDodo(true) ;
@@ -522,7 +513,9 @@ const widthPoids = counterPoids.interpolate({
     </DraxProvider>
     </GestureHandlerRootView>
   );
-}
+        }
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
